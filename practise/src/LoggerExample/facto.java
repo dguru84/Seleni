8854d0622ea607dbeef904ad03ad7068b4d981d4
Guru <dguru84@gmail.com>
2016-09-26 19:51:36 +0530

    Committing...1

--- practise/src/LoggerExample/facto.java
+++ practise/src/LoggerExample/facto.java
@@ -6,13 +6,8 @@ public class facto {
 
 		public static void main(String args[]){
 
-//		Scanner in = new Scanner(System.in);
-//
-//		System.out.println(“enter the num for which u want the factorial”);
-//
-//		int num = in.nextInt();
 			
-			int num = 3;
+		int num = 3;
 
 		for(int i=num-1; i>0; i-- ){
 
